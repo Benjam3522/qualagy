cb2ee92101dfd4dd1f99ba9f5d854e4bea13e6b0
Benjamin Justin <benjaminjustind@gmail.com>
2026-06-27 01:14:21 +0100

    interpreter added

---
+++ interpreter.py
@@ -0,0 +1,139 @@
+class Interpreter:
+    def __init__(self):
+        self.variables = {}
+
+    def execute(self, statements):
+        for stmt in statements:
+            self.visit(stmt)
+
+    def visit(self, node):
+        method_name = f"visit_{type(node).__name__}"
+        method = getattr(self, method_name, None)
+
+        if method is None:
+            raise RuntimeError(
+            f"No visitor for {type(node).__name__}"
+         )
+
+        return method(node)
+        
+    def visit_Number(self, node):
+        return node.value
+    
+    def visit_String(self, node):
+        return node.value.strip('"')
+    
+    def visit_Boolean(self, node):
+        return node.value
+    
+    def visit_Null(self, node):
+        return None
+
+    def visit_Identifier(self, node):
+        if node.name not in self.variables:
+            raise RuntimeError(
+                f"Undefined variable: {node.name}"
+            )
+
+        return self.variables[node.name]
+
+    def visit_VarAssign(self, node):
+        value = self.visit(node.value)
+        self.variables[node.name] = value
+
+    def visit_Assign(self, node):
+        if node.name not in self.variables:
+            raise RuntimeError(
+                f"Variable '{node.name}' does not exist."
+            )
+
+        value = self.visit(node.value)
+        self.variables[node.name] = value
+
+    def visit_BinaryOp(self, node):
+        left = self.visit(node.left)
+        right = self.visit(node.right)
+
+        if node.op == "PLUS":
+            return left + right
+
+        if node.op == "MINUS":
+            return left - right
+
+        if node.op == "STAR":
+            return left * right
+
+        if node.op == "SLASH":
+            return left / right
+
+        if node.op == "PERCENT":
+            return left % right
+
+        if node.op == "EQ":
+            return left == right
+
+        if node.op == "NE":
+            return left != right
+
+        if node.op == "GT":
+            return left > right
+
+        if node.op == "GE":
+            return left >= right
+
+        if node.op == "LT":
+            return left < right
+
+        if node.op == "LE":
+            return left <= right
+        
+    def visit_UnaryOp(self, node):
+        value = self.visit(node.operand)
+
+        if node.op == "PLUS":
+            return +value
+
+        if node.op == "MINUS":
+            return -value
+        
+    def visit_SimpleStmt(self, node):
+        return self.visit(node.expr)
+
+    def visit_Block(self, node):
+        result = None
+        for stmt in node.statements:
+            result = self.visit(stmt)
+        return result
+
+    def visit_If(self, node):
+        if self.visit(node.condition):
+            return self.visit(node.then_branch)
+
+        for cond, branch in node.elif_branches:
+            if self.visit(cond):
+                return self.visit(branch)
+
+        if node.else_branch:
+            return self.visit(node.else_branch)
+        
+    def visit_While(self, node):
+        while self.visit(node.condition):
+            self.visit(node.body)
+
+    def visit_Call(self, node):
+        if getattr(node.callee, "name", None) == "write":
+            values = []
+
+            for arg in node.args:
+                values.append(
+                    str(self.visit(arg))
+                )
+
+            print(*values)
+
+            return None
+
+        raise RuntimeError(
+            f"Unknown function: {node.callee.name}"
+        )
+        
--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 from lexer import lex
 from parser import Parser
+from interpreter import Interpreter
 
 code = """
 var x = 5 + 2 * 3
 
-if x > 10 { 
-    write("x is greater than 10") 
-} 
-else { 
-    write("x is less than or equal to 10") 
+if x > 10 {
+    write("x is greater than 10")
+}
+else {
+    write("x is less than or equal to 10")
 }
 """
 
+# 1. Lexing
 tokens = lex(code)
+
+print("=== TOKENS ===")
+for token in tokens:
+    print(token)
+
+# 2. Parsing
 parser = Parser(tokens)
 ast = parser.parse()
 
-print(ast)
+print("\n=== AST ===")
+for node in ast:
+    print(node)
+
+# 3. Interpreting
+print("\n=== OUTPUT ===")
+interpreter = Interpreter()
+interpreter.execute(ast)
